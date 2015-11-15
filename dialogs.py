f1f9891984408aaf39d5731ca6d59942e28d3cb7
xor <xor@users.noreply.github.com>
2015-11-15 18:18:23 +0000

    Added default responses for the dialogs, and added a new dialog to edit accounts.

--- dialogs.py
+++ dialogs.py
@@ -12,16 +12,17 @@ class Add(Gtk.Dialog):
     Add Dialog
     '''
     
-    title = 'New Password'
+    title = 'New Account'
     
     def __init__(self, app):
-        buttons = ('_Cancel', Gtk.ResponseType.CANCEL,
-                   '_OK', Gtk.ResponseType.OK)
+        buttons = ('_OK', Gtk.ResponseType.OK,
+                   '_Cancel', Gtk.ResponseType.CANCEL)
         properties = {'use_header_bar': True}
         super().__init__(None, app.window, 0, buttons, **properties)
         # I set the title on the next line instead of the constructor because
         # this way the window width is recalculated to show the entire title.
         self.get_header_bar().set_custom_title(Gtk.Label(self.title))
+        self.set_default_response(Gtk.ResponseType.OK)
         
         grid = Gtk.Grid()
         grid.set_column_spacing(app.spacing)
@@ -37,6 +38,7 @@ class Add(Gtk.Dialog):
         
         label = Gtk.Label('Service')
         self.service = Gtk.Entry()
+        self.service.set_activates_default(True)
         self.service.set_hexpand(True)
         grid.attach(label, 0, 1, 1, 1)
         grid.attach(self.service, 1, 1, 1, 1)
@@ -44,6 +46,7 @@ class Add(Gtk.Dialog):
         
         label = Gtk.Label('Username')
         self.username = Gtk.Entry()
+        self.username.set_activates_default(True)
         grid.attach(label, 0, 2, 1, 1)
         grid.attach(self.username, 1, 2, 1, 1)
         
@@ -52,6 +55,7 @@ class Add(Gtk.Dialog):
                 'input-purpose': Gtk.InputPurpose.PASSWORD,
                 'visibility': False}
         self.password = Gtk.Entry(**args)
+        self.password.set_activates_default(True)
         grid.attach(label, 0, 3, 1, 1)
         grid.attach(self.password, 1, 3, 1, 1)
         
@@ -60,6 +64,7 @@ class Add(Gtk.Dialog):
         scrolled_window.set_min_content_height(64)
         scrolled_window.set_vexpand(True)
         self.notes = Gtk.TextView()
+        self.notes.set_accepts_tab(False)
         scrolled_window.add(self.notes)
         grid.attach(label, 0, 4, 1, 1)
         grid.attach(scrolled_window, 1, 4, 1, 1)
@@ -68,4 +73,21 @@ class Add(Gtk.Dialog):
         box.add(grid)
         
         self.show_all()
+
+
+class Edit(Add):
+    '''
+    Edit Dialog
+    '''
+    
+    title = 'Edit Account'
+    
+    def __init__(self, app, item):
+        super().__init__(app)
+        attributes = item.get_attributes()
+        self.service.set_text(attributes['service'])
+        self.username.set_text(attributes['username'])
+        self.notes.get_buffer().set_text(attributes['notes'])
+        item.load_secret_sync()
+        self.password.set_text(item.get_secret().get_text())
 
