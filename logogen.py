fa0860452cbc7680166d729af94803d66ac4b9bb
xor <xor@users.noreply.github.com>
2015-11-27 10:56:27 +0000

    Created a little helper class to create logos for the main view.

---
+++ logogen.py
@@ -0,0 +1,31 @@
+'''
+Module for the LogoGen class
+'''
+
+from gi import require_version
+require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio
+import dialogs
+
+
+class LogoGen:
+    '''
+    LogoGen class
+    '''
+    
+    def __init__(self, app, service, username):
+        settings = Gio.Settings(app.schema_id + '.preferences.logo')
+        size = settings['size']
+        icon_theme = Gtk.IconTheme.get_default()
+        pixbuf = icon_theme.load_icon('image-missing', size * 32, 0)
+        image = Gtk.Image.new_from_pixbuf(pixbuf)
+        label_text = '<b>{}:</b> {}'.format(service, username)
+        if size == 1:
+            label_text = '<small>{}</small>'.format(label_text)
+        elif size == 3:
+            label_text = '<big>{}</big>'.format(label_text)
+        label = Gtk.Label(label_text, **{'use-markup': True})
+        self.box = Gtk.Box(Gtk.Orientation.HORIZONTAL)
+        self.box.pack_start(image, False, False, 0)
+        self.box.pack_start(label, True, False, 0)
+
