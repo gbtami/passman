28e00be2ab6b0fd8b5b14e99298bf53764c0078a
xor <xor@users.noreply.github.com>
2015-11-30 12:52:31 +0000

    I can now generate the logo based on the view mode and size selected.

--- logogen.py
+++ logogen.py
@@ -14,18 +14,64 @@ class LogoGen:
     '''
     
     def __init__(self, app, service, username):
-        settings = app.settings.get_child('logo')
-        size = settings['size']
+        self.service = service
+        self.username = username
+        size = app.logo_size
+        self.size = size
+        view_mode = app.view_mode
+        self.mode = view_mode
+        self.grid = Gtk.Grid()
+        self.image = Gtk.Image()
+        self.label = Gtk.Label()
+        self.label.set_justify(Gtk.Justification.CENTER)
+        self.label.set_hexpand(True)
+        self.grid.add(self.image)
+        self.grid.add(self.label)
+        self.set_view(view_mode)
+        self.set_size(size)
+    
+    def set_view(self, mode):
+        self.grid.remove(self.label)
+        self.mode = mode
+        if mode == 'list':
+            self.grid.set_halign(Gtk.Align.FILL)
+            self.grid.set_orientation(Gtk.Orientation.HORIZONTAL)
+            self.separator = '<b>: </b>'
+            self.size_open = '<big>'
+            self.size_close = '</big>'
+        else:
+            self.grid.set_halign(Gtk.Align.CENTER)
+            self.grid.set_orientation(Gtk.Orientation.VERTICAL)
+            self.separator = '\n'
+            self.size_open = ''
+            self.size_close = ''
+        self.grid.add(self.label)
+        self._set_label()
+        self._set_image()
+    
+    def set_size(self, size):
+        self.size = size
+        self._set_image()
+        self._set_label()
+    
+    def _set_image(self):
         icon_theme = Gtk.IconTheme.get_default()
-        pixbuf = icon_theme.load_icon('image-missing', size * 32, 0)
-        image = Gtk.Image.new_from_pixbuf(pixbuf)
-        label_text = '<b>{}:</b> {}'.format(service, username)
-        if size == 1:
-            label_text = '<small>{}</small>'.format(label_text)
-        elif size == 3:
-            label_text = '<big>{}</big>'.format(label_text)
-        label = Gtk.Label(label_text, **{'use-markup': True})
-        self.box = Gtk.Box()
-        self.box.pack_start(image, False, False, 0)
-        self.box.pack_start(label, True, False, 0)
+        if self.mode == 'list':
+            size = self.size * 24
+        else:   
+            size = self.size * 48
+        pixbuf = icon_theme.load_icon('image-missing', size, 0)
+        self.image.set_from_pixbuf(pixbuf)
+    
+    def _set_label(self):
+        text = '{}<b>{}</b>{}{}{}'.format(self.size_open,
+                                          self.service,
+                                          self.separator,
+                                          self.username,
+                                          self.size_close)
+        if self.size == 1:
+            text = '<small><small>{}</small></small>'.format(text)
+        elif self.size == 3:
+            text = '<big><big>{}</big></big>'.format(text)
+        self.label.set_markup(text)
 
