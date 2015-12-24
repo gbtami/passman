91d68e24d479509b1bf3390d101be4fb5b81045e
xor <xor@users.noreply.github.com>
2015-12-24 10:56:25 +0000

    Brought view_mode and view_size inside the class, and added the unrealize signal handler to store those changes on gsettings.

--- header_bar.py
+++ header_bar.py
@@ -22,6 +22,7 @@ class HeaderBar(Gtk.HeaderBar):
         self.app = app
         self.set_title(app.title)
         self.set_show_close_button(True)
+        self.connect('unrealize', self.on_unrealize)
         
         button = Gtk.Button()
         button.connect('clicked', self.on_add)
@@ -30,12 +31,14 @@ class HeaderBar(Gtk.HeaderBar):
         button.add(image)
         self.pack_start(button)
         
-        state = app.view_mode
-        action = Gio.SimpleAction(name='view_mode',
+        view_settings = app.settings.get_child('view')
+        self.view_mode = view_settings['mode']
+        self.view_size = view_settings['size']
+        action = Gio.SimpleAction(name='view_mode_switch',
                                   parameter_type=GLib.VariantType('s'),
-                                  state=GLib.Variant('s', state))
+                                  state=GLib.Variant('s', self.view_mode))
         app.add_action(action)
-        action.connect('activate', self.view_mode)
+        action.connect('activate', self.on_view_mode)
         
         button = Gtk.MenuButton()
         builder = Gtk.Builder.new_from_file(self.app.gui_ui)
@@ -46,13 +49,13 @@ class HeaderBar(Gtk.HeaderBar):
         button.add(image)
         self.pack_end(button)
         
-        scale = builder.get_object('scale')
-        scale.set_value(app.logo_size)
-        scale.connect('value-changed', self.on_value_changed)
+        self.scale = builder.get_object('scale')
+        self.scale.set_value(self.view_size)
+        self.scale.connect('value-changed', self.on_value_changed)
         self.show_all()
     
-    def view_mode(self, action, target):
-        self.app.view_mode = target.get_string()
+    def on_view_mode(self, action, target):
+        self.view_mode = target.get_string()
         action.change_state(target)
         flowbox = self.app.main_view.flowbox
         if target == GLib.Variant('s', 'list'):
@@ -68,7 +71,7 @@ class HeaderBar(Gtk.HeaderBar):
     
     def on_value_changed(self, scale):
         size = int(scale.get_value())
-        self.app.logo_size = size
+        self.view_size = size
         for c in self.app.main_view.flowbox.get_children():
             button = c.get_child()
             button.logo.set_size(size)
@@ -83,4 +86,9 @@ class HeaderBar(Gtk.HeaderBar):
             button = self.app.main_view.create_button(item)
             self.app.main_view.insert_button(button)
         dialog.destroy()
+    
+    def on_unrealize(self, widget):
+        view_settings = self.app.settings.get_child('view')
+        view_settings.set_value('size', GLib.Variant('q', self.view_size))
+        view_settings.set_string('mode', self.view_mode)
 
