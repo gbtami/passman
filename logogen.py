52529c4dc277dfd35f67a4bf717ec7e5ea0c9a14
xor <xor@users.noreply.github.com>
2016-01-04 17:49:17 +0000

    Added code to create a logo name cache, to update the logo based on local data, and update the logo based on remote data.

--- logogen.py
+++ logogen.py
@@ -4,10 +4,19 @@
 Module for the LogoGen class
 '''
 
+import os
+import io
+import bz2
+import tarfile
+import pickle
+import threading
+
+import requests
+
 from gi import require_version
 require_version('Gtk', '3.0')
 require_version('Pango', '1.0')
-from gi.repository import Gtk, Gio, Pango
+from gi.repository import Gtk, Gio, GLib, Pango
 
 import dialogs
 
@@ -17,13 +26,17 @@ class LogoGen:
     LogoGen class
     '''
     
-    def __init__(self, app, service, username):
+    logo_size = 128
+    logo_server = 'http://localhost:8080/logo_server/'
+    
+    def __init__(self, app, service, username, size, mode,
+                 refresh_function=lambda:None):
+        self.app = app
         self.service = service
         self.username = username
-        size = app.window.get_titlebar().view_size
         self.size = size
-        view_mode = app.window.get_titlebar().view_mode
-        self.mode = view_mode
+        self.mode = mode
+        self.refresh_function = refresh_function
         self.grid = Gtk.Grid()
         self.image = Gtk.Image()
         self.label = Gtk.Label()
@@ -33,8 +46,70 @@ class LogoGen:
         self.label.set_max_width_chars(4)
         self.grid.add(self.image)
         self.grid.add(self.label)
-        self.set_view(view_mode)
-        self.set_size(size)
+        self.set_view(self.mode)
+        self.set_size(self.size)
+        
+        self.load_cache()
+        icon_theme = Gtk.IconTheme.get_default()
+        if mode == 'grid':
+            size = size * 32
+        else:
+            size = size * 24
+        self.missing_image = icon_theme.load_icon('image-missing', size, 0)
+    
+    def load_cache(self):
+        path = self.app.data_dir / 'logo_name_cache.bz2'
+        with bz2.open(str(path), 'rb') as logo_name_cache_file:
+            logo_name_cache_bytes = logo_name_cache_file.read()
+            self.logo_name_cache = pickle.loads(logo_name_cache_bytes)
+    
+    def update_local(self, logo_name):
+        if logo_name in self.logo_name_cache:
+            if not self.get_local_logo(logo_name):
+                self.update_remote(logo_name)
+                return False
+        else:
+            self.image.set_from_pixbuf(self.missing_image)
+        self.refresh_function()
+        return True
+    
+    def update_remote(self, logo_name):
+        target = self.update_remote_worker
+        kwargs = {'logo_name': logo_name}
+        thread = threading.Thread(target=target, kwargs=kwargs)
+        thread.daemon = True
+        thread.start()
+    
+    def get_local_logo(self, logo_name):
+        if logo_name in os.listdir(str(self.app.img_dir)):
+            if logo_name == self.service:
+                path = str(self.app.img_dir / logo_name / 'logo.png')
+                self.image.set_from_file(path)
+                self.refresh_function()
+            return True
+        return False
+    
+    def wrap_get_local_logo(self, logo_name):
+        '''
+        I need this wrapper to call this method from GLib.idle_add.
+        So that it will automatically stop when it returns False.
+        I need the actual get_local_logo method to return True sometimes.
+        '''
+        self.get_local_logo(logo_name)
+        return False
+    
+    def update_remote_worker(self, logo_name):
+        import time
+        time.sleep(2)
+        url = '{}{}'.format(self.logo_server, logo_name)
+        r = requests.get(url)
+        if r.status_code == requests.codes.ok:
+            with io.BytesIO(r.content) as in_memory_file:
+                logo_file = tarfile.open(fileobj=in_memory_file, mode='r:')
+                path = str(self.app.data_dir)
+                logo_file.extractall(path=path)
+                logo_file.close()
+        GLib.idle_add(self.wrap_get_local_logo, logo_name)
     
     def set_view(self, mode):
         self.grid.remove(self.label)
