33276e147a9ce2165b5b1a04154f6a52ee6b837d
xor <xor@users.noreply.github.com>
2016-01-18 14:00:54 +0000

    Starting to work out how to package this.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='passman',
+      version='0.1.0',
+      description='Easy to use password manager.',
+      url='https://github.com/xor/passman',
+      author='Pedro Azevedo',
+      author_email='passman@idlecore.com',
+      license='GPL3',
+      packages=find_packages())
+
