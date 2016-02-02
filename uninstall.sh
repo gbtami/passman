4397ba5266873287d3f747a7c260fd78f25658bb
xor <xor@users.noreply.github.com>
2016-02-02 02:50:33 +0000

    Added a line to remove the PassMan keyring, but commented it out. I want to leave there a way to remove it, but I don't expect I'll ever actually uncomment it.

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,5 @@ rm /usr/share/applications/passman.desktop
 rm /usr/share/glib-2.0/schemas/com.idlecore.passman.gschema.xml
 rm -r /usr/lib/python3.4/site-packages/passman
 rm -r /usr/share/help/C/passman
+# I'm not sure I ever want to automatically delete the keyring
+# rm -r ~/.local/share/keyrings/passman.keyring
