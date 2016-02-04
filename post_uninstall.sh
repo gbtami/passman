d7ea30fa2e2307d82adac842db848d00c2c84ef0
xor <xor@users.noreply.github.com>
2016-02-04 00:17:07 +0000

    Remove help files on uninstall, and added the bash shebang.

--- post_uninstall.sh
+++ post_uninstall.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 rm -r /home/xor/.config/autostart/passman-autostart.desktop 2>&1|logger
 rm -r /home/xor/.local/share/passman 2>&1|logger
 rm -r /usr/lib/python3.4/site-packages/passman 2>&1|logger
+rm -r /usr/share/help/C/passman 2>&1|logger
