1554025a9600184a9e6fc03c666e036774550390
xor <xor@users.noreply.github.com>
2016-02-05 03:45:29 +0000

    Added /usr/share/passman

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 rm -r /home/xor/.config/autostart/passman-autostart.desktop
 rm -r /home/xor/.local/share/passman
 rm /usr/bin/passman
+rm -r /usr/share/passman
 rm /usr/share/applications/passman.desktop
 rm /usr/share/glib-2.0/schemas/com.idlecore.passman.gschema.xml
 rm -r /usr/lib/python3.4/site-packages/passman
