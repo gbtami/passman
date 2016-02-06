85976fa9d4c4e9379e3184e05ee5367e362aa155
xor <xor@users.noreply.github.com>
2016-02-06 08:26:18 +0000

    This was actually just a little tool I used to test my code locally, I'm not sure I want people looking at it and wondering if it's actually the entry point. Better just remove it.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo rpm -e PassMan
-python3 setup.py bdist_rpm --post-install post_install.sh \
-                           --post-uninstall post_uninstall.sh
-sudo rpm -i dist/PassMan-0.1.0-1.noarch.rpm
-passman
