dff8bb120b1c647fbbca9b19254a4a83057c4575
xor <xor@users.noreply.github.com>
2016-02-06 08:32:17 +0000

    Added a few more paths that are used in debian based systems.

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,15 @@
+# This little file just helps me track of all the files that are added
+# So if I ever want a clean system, I can just remove everything.
 rm -r /home/xor/.config/autostart/passman-autostart.desktop
 rm -r /home/xor/.local/share/passman
 rm /usr/bin/passman
 rm -r /usr/share/passman
 rm /usr/share/applications/passman.desktop
 rm /usr/share/glib-2.0/schemas/com.idlecore.passman.gschema.xml
-rm -r /usr/lib/python3.4/site-packages/passman
 rm -r /usr/share/help/C/passman
+rm -r /usr/lib/python3.4/site-packages/passman
+rm -r /usr/lib/python3.4/site-packages/PassMan*
+rm -r /usr/lib/python3.4/dist-packages/passman
+rm -r /usr/lib/python3.4/dist-packages/PassMan*
 # I'm not sure I ever want to automatically delete the keyring
 # rm -r ~/.local/share/keyrings/passman.keyring
