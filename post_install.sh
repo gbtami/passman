f493f598d195c9448febad9e29e180c5b2d13142
xor <xor@users.noreply.github.com>
2016-02-07 11:05:17 +0000

    Used xdg directories instead of hardcoded.

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-# Output stdout and stderr to journal.
-glib-compile-schemas /usr/share/glib-2.0/schemas 2>&1|logger
+#!/bin/sh
+
+system_data_dir=`python3 -c 'from gi.repository import GLib; \
+                             print(GLib.get_system_data_dirs()[-1]);'`
+glib-compile-schemas $system_data_dir/glib-2.0/schemas 2>&1|logger
 
