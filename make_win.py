15d7e60b5f1667562b7466816776e3c9fe4f79a9
xor <xor@users.noreply.github.com>
2016-02-11 04:18:31 +0000

    Added helper script to create windows port.

---
+++ make_win.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system('cp schema/com.idlecore.passman.gschema.xml '
+          'windows/pynsist_pkgs/gnome/share/glib-2.0/schemas/')
+
+os.system('glib-compile-schemas '
+          'windows/pynsist_pkgs/gnome/share/glib-2.0/schemas/')
+
+os.system('pynsist windows/installer.cfg')
