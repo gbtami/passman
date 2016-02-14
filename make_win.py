9dd3146cb8515fc36ec45ddc207d87ed702e7968
xor <xor@users.noreply.github.com>
2016-02-14 01:05:14 +0000

    The pynsist_pkgs directory is kept outside the windows directory now.

--- make_win.py
+++ make_win.py
@@ -4,9 +4,9 @@
 import os
 
 os.system('cp schema/com.idlecore.passman.gschema.xml '
-          'windows/pynsist_pkgs/gnome/share/glib-2.0/schemas/')
+          'pynsist_pkgs/gnome/share/glib-2.0/schemas/')
 
 os.system('glib-compile-schemas '
-          'windows/pynsist_pkgs/gnome/share/glib-2.0/schemas/')
+          'pynsist_pkgs/gnome/share/glib-2.0/schemas/')
 
 os.system('pynsist installer.cfg')
