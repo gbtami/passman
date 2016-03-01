ff5ceff8fb019992ab1c53d9cce21eb816159f42
xor <xor@users.noreply.github.com>
2016-03-01 09:02:12 +0000

    Create item. This isn't tested yet.

--- windows/libcred.py
+++ windows/libcred.py
@@ -5,7 +5,43 @@ Module for the Windows Credentials util library
 '''
 
 import ctypes
-from ctypes.wintypes import DWORD, LPTSTR, FILETIME, LPBYTE
+from ctypes.wintypes import DWORD, LPTSTR, FILETIME, LPBYTE, \
+                            PCREDENTIAL_ATTRIBUTE
+
+
+class Credential(ctypes.Structure):
+    '''
+    Credential ctypes structure
+    '''
+
+    _fields_ = [('Flags', DWORD),
+                ('Type', DWORD),
+                ('TargetName', LPTSTR),
+                ('Comment', LPTSTR),
+                ('LastWritten', FILETIME),
+                ('CredentialBlobSize', DWORD),
+                ('CredentialBlob', LPBYTE),
+                ('Persist', DWORD),
+                ('AttributeCount', DWORD),
+                ('Attributes', PCREDENTIAL_ATTRIBUTE),
+                ('TargetAlias', LPTSTR),
+                ('UserName', LPTSTR)]
+
+
+class CredAttributes(ctypes.Structure):
+    '''
+    Credential Attributes ctypes structure
+    '''
+    
+    _fields_ = [('logo', LPTSTR)]
+
+
+class CredBlob(ctypes.Structure):
+    '''
+    Credential Blob ctypes structure
+    '''
+    
+    _fields_ = [('password', LPTSTR), ('notes', LPTSTR)]
 
 
 class LibCred:
@@ -13,7 +49,13 @@ class LibCred:
     LibCred class
     '''
     
-    def __init__(self, app):
+    advapi32 = ctypes.CDLL('Advapi32.dll')
+    CRED_TYPE_GENERIC = DWORD(1)
+    # Windows Vista Home Basic, Windows Vista Home Premium, Windows Vista
+    # Starter, and Windows XP Home Edition:  This value is not supported.
+    CRED_PERSIST_LOCAL_MACHINE = DWORD(2)
+    
+    def __init__(self):
         pass
     
     def get_collection(self):
@@ -29,10 +71,30 @@ class LibCred:
         pass
     
     def create_item(self, logo, service, username, password, notes):
-        pass
+        cred_attributes_type = CredAttributes * 1
+        attributes = cred_attributes_type(logo)
+        blob = CredBlob(password, notes)
+        cred_flags = DWORD(0)
+        cred_type = self.CRED_TYPE_GENERIC
+        cred_target_name = LPTSTR(service)
+        cred_comment = LPTSTR('')
+        cred_last_written = FILETIME(0)
+        cred_credential_blob_size = ctypes.sizeof(blob)
+        cred_credential_blob = ctypes.byref(blob)
+        cred_persist = self.CRED_PERSIST_LOCAL_MACHINE
+        cred_attribute_count = DWORD(len(attributes))
+        cred_attributes = ctypes.byref(attributes)
+        cred_target_alias = LPTSTR('passman')
+        cred_username = LPTSTR(username)
+        cred = Credential(cred_flags, cred_type, cred_target_name,
+                          cred_comment, cred_last_written,
+                          cred_credential_blob_size, cred_credential_blob,
+                          cred_persist, cred_attribute_count, cred_attributes,
+                          cred_target_alias, cred_username)
+        self.advapi32.CredWrite(ctypes.byref(cred), 0)
     
-    def edit_item(self, item, logo, service, username, password, notes):
-        pass
+    def edit_item(self, logo, service, username, password, notes):
+        pass  
     
     def delete_item(self, item):
         pass
@@ -76,30 +138,3 @@ class LibCred:
         '''
         pass
 
-
-class CREDENTIAL(ctypes.Structure):
-    '''
-    Main Application class
-    '''
-
-    _fields_ = [("Flags", DWORD),
-                ("Type", DWORD),
-                ("TargetName", LPTSTR),
-                ("Comment", LPTSTR),
-                ("LastWritten", FILETIME),
-                ("CredentialBlobSize", DWORD),
-                ("CredentialBlob", LPBYTE),
-                ("Persist", DWORD),
-                ("AttributeCount", DWORD),
-                ("Attributes", ctypes.c_wchar_p),
-                ("TargetAlias", LPTSTR),
-                ("UserName", LPTSTR)]
-
-advapi32 = ctypes.CDLL('Advapi32.dll')
-advapi32.CredReadW.restype = ctypes.c_bool
-advapi32.CredReadW.argtypes = [ctypes.c_wchar_p, ctypes.c_int, ctypes.c_int, ctypes.POINTER(CREDENTIAL)]
-target = "login.example.com"
-pcred = ctypes.pointer(CREDENTIAL())
-ok = advapi32.CredReadW(target,1,0,pcred)
-cred = pcred.contents
-print(ok, pcred, cred.UserName, cred.CredentialBlob)
