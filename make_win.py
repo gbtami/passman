3e6c89dc3c5a2bb9977e6bbbf98d636eb91f8f8e
xor <xor@users.noreply.github.com>
2016-04-01 07:48:08 +0100

    Instead of removing the collection help page, just add some text explaining it works on Linux only.

--- make_win.py
+++ make_win.py
@@ -9,13 +9,6 @@ os.system('cp schema/com.idlecore.passman.gschema.xml '
 os.system('glib-compile-schemas '
           'pynsist_pkgs/gnome/share/glib-2.0/schemas/')
 
-# I choose the files individually because on Windows there is no need for the
-# collection.html file, I could just individually remove it but the index.html
-# file would include a link to it anyway. This way collection.page isn't
-# present during building so index.html won't create a link to it.
-os.system('yelp-build html -o help help/delete.page help/edit.page \
-           help/general.page help/index.page help/mode.page help/new.page \
-           help/passwords.page help/shortcuts.page help/size.page \
-           help/tutorial.page')
+os.system('yelp-build html -o help help/*.page')
 
 os.system('pynsist installer.cfg')
