a7a3788a0787b48afb27a586bf168a33883c0cd7
xor <xor@users.noreply.github.com>
2016-05-14 09:14:03 +0100

    Update the TODO a bit. Added some extra code to the windows binary script to clean make html help files for all languages properly and clean up after.

--- make_win.py
+++ make_win.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pathlib
 
 os.system('cp schema/com.idlecore.passman.gschema.xml '
           'pynsist_pkgs/gnome/share/glib-2.0/schemas/')
@@ -12,8 +13,16 @@ os.system('glib-compile-schemas '
 # Build all except collection.page, it's not necessary on Windows.
 # It's essential the collection.page file isn't built. Removing the file
 # after a full build still leaves links to that file on index.html.
-os.system('ls help/*.page | '
-          'grep -v collection.page | '
-          'xargs yelp-build html -o help')
+help_path = pathlib.Path('help')
+help_walk = os.walk(str(help_path))
+dirpath, dirnames, filenames = next(help_walk)
+for directory in dirnames:
+    language_path = str(help_path / directory / 'passman')
+    os.system('find ' + language_path + ' -name *.page | '
+              'grep -v collection.page | '
+              'xargs yelp-build html -o ' + language_path)
 
 os.system('pynsist installer.cfg')
+
+os.system('find help -name *.html -o -name *.js -o -name *.css | xargs rm')
+
